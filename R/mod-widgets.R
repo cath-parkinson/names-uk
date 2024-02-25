7d4fc988151f1700cefb8abc6e991122e807a61c
Cath Parkinson <cparkinson15@gmail.com>
2024-02-25 19:05:42 +0000

    Populate the `widgets_ui`.

--- R/mod-widgets.R
+++ R/mod-widgets.R
@@ -2,16 +2,56 @@
 
 widgets_ui <- function(id){
   
+  ns <- NS(id)
   
+  tagList(
+    
+    # Testing
+    actionBttn(
+      inputId = ns("debug"),
+      label = "debug"
+    ),
+    
+    radioGroupButtons(
+      inputId = ns("radio_select_gender"),
+      label = "Select gender(s):",
+      choices = c("Girl", "Boy", "Both")
+    ),
+    
+    pickerInput(
+      inputId = ns("select_names"),
+      label = "Select name(s):",
+      choices = NULL,
+      multiple = TRUE,
+      options = list(
+        `live-search` = TRUE
+      )
+    )
+    
+  )
   
 }
 
-widgets_server <- function(id){
+widgets_server <- function(id,
+                           name_data){
   
   moduleServer(id, function(input, output, session){
     
+    # Testing
+    observeEvent(input$debug, {
+      browser()
+    })
     
-  }
+    observe({
+      
+      updatePickerInput(session,
+                        inputId = "select_names",
+                        choices = name_data %>% dplyr::pull(name) %>% unique(),
+                        selected = (name_data %>% dplyr::pull(name) %>% unique())[1])
+      
+    })
+    
+  })
   
 }
 
