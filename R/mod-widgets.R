ae0f0337c4fdeff07ed1ae2434a59ce71f17d590
Cath Parkinson <cparkinson15@gmail.com>
2024-02-25 18:41:47 +0000

    Module shell for the `widgets_ui`

---
+++ R/mod-widgets.R
@@ -0,0 +1,18 @@
+# Module: Widgets allowing the user to update the charts
+
+widgets_ui <- function(id){
+  
+  
+  
+}
+
+widgets_server <- function(id){
+  
+  moduleServer(id, function(input, output, session){
+    
+    
+  }
+  
+}
+
+
